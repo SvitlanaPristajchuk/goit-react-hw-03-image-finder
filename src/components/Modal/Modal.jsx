02f938d657681078c8c82d38dcee3b3574d444e0
Pristajchuk-Humain <73787800+SvitlanaPristajchuk@users.noreply.github.com>
2022-07-21 14:23:00 +0200

    new file

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,1 @@
+import { Form, Formik, Field } from 'formik';
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,6 +1,34 @@
-import { Form, Formik, Field } from 'formik';
+
+  import React from 'react';
+  import { Formik, Form, Field, ErrorMessage } from 'formik';
+  
+  const Searchbar = ({onSubmit, isSubmiting}) => {
+    const handleSubmit = (values, actions) => {
+      onSubmit(values)
+      actions.resetForm();
 
 
 
+      
 
-export default Searchbar
+    }
+    return (
+      <Formik
+        initialValues={{ search: "" }}
+        onSubmit={handleSubmit}>
+          <Form>
+            <Field type="text" name="search" />
+            <button type="submit" desebled={isSubmiting}>
+
+
+              Submit
+            </button>
+          </Form>
+      </Formik>
+
+    )
+  }
+  
+  export default Searchbar
+
+

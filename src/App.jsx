ba4fb91ec7af6d487f974382642ff61c1996ed39
Svetlana Pristaychuk <svetlana.pristajchuk@gmail.com>
2022-07-21 06:45:07 +0200

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import React, { Component } from 'react';
+
+import "./App.css";
+import { Component } from "react";
+import axios from "axios";
+import Searchbar from "./components/Searchbar/Searchbar";
+import { Formik } from "formik";
+
+//axios.defaults.baseURL = `https://pixabay.com/27144211-7692123c331450892536fcf0f/?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12`;
 
 
-class App extends Component 
 
 
-
-
-
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+      <Searchbar />
+      </>
+    );
+  }
 }
+
+
 
 export default App;
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,1 +1,17 @@
+import { Form, Formik, Field } from 'formik';
 
+
+ const Searchbar = () => {
+    return {
+    <Formik
+     initialValues={{ title: '', link: '' }}
+     onSubmit={(yyy = ( () => (console.log"yyy")))}>
+        <Field>Jgbcfzbe</Field>
+    <Form>
+        <button type="submit">ghkj.l</button>
+    </Form>
+    </Formik>
+};
+ }
+
+export default Searchbar

71a4ab294f878954bf3695ed94d162beda3c3b2e
Pristajchuk-Humain <73787800+SvitlanaPristajchuk@users.noreply.github.com>
2022-07-25 15:30:15 +0200

    komitt

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -2,9 +2,11 @@ import axios from "axios";
 
 axios.defaults.baseURL = 'https://pixabay.com/27144211-7692123c331450892536fcf0f/?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12';
 
- export const  getImages = async (values) => {
-    const response = await axios.get('/images')
-}
+export const getImages = async () => {
+    const response = await axios.get('/values');
+    return response.data;
+  };
+
 
 
 
